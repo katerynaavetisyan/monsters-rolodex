97ef8454b72f28cbcc6178aeebb80e145d29e983
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-06-04 16:13:59 -0500

    set state and secondary callback

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ constructor(){
   super();
 
   this.state={
-    name:'Yihua',
+    name: {firstName:'Yihua', lastName:'Zhand'},
     company: 'AAA Service'
   }
 }
@@ -18,10 +18,10 @@ constructor(){
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p> Hi {this.state.name}, I work at {this.state.company} </p>
+          <p> Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company} </p>
           
          <button onClick={()=>{
-          this.setState({name:'Andrei'});
+          this.setState({name:{firstName:'Andrei', lastName:'Neagoi'}});
           console.log(this.state)
          }}
          >
