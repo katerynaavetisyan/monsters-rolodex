0a38d8508eeba6e5123e9407d7c8799e3147973e
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-06-05 16:22:29 -0500

    mapping arrays to elements

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import {Component} from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-constructor(){
-  super();
+  constructor() {
+    super();
 
-  this.state = {
-    monster1:{
-      name:'Kate'
-    },
-    monster2:{
-      name:'Frank'
-    },
-    monster3:{
-      name:'Jacky'
-    }
+    this.state = {
+      monsters: [
+        {
+          name: "Kate",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+        {
+          name: "Artur",
+        },
+      ],
+    };
   }
-}
 
-  render(){
+  render() {
     return (
       <div className="App">
-
-      <h1>{this.state.monster1.name}</h1>
-      <h1>{this.state.monster2.name}</h1>
-      <h1>{this.state.monster3.name}</h1>
-      
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
-  
 }
 
 export default App;
