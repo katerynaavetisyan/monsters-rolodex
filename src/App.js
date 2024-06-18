3fbfcba303402840ec65d04ac7bcb5521c7495dd
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-06-18 15:44:59 -0500

    jsx

--- src/App.js
+++ src/App.js
@@ -3,39 +3,43 @@ import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
+// class App extends Component {
+//   constructor() {
+//     super();
 
-    this.state = {
-      monsters: [],
-    };
-  }
+//     this.state = {
+//       monsters: [],
+//     };
+//   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//           },
+//           () => {
+//             console.log(this.state);
+//           }
+//         )
+//       );
+//   }
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
-        })}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="App">
+//         {this.state.monsters.map((monster) => {
+//           return <h1 key={monster.id}>{monster.name}</h1>;
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  return <h1>Hello JSX!</h1>;
+};
 
 export default App;
