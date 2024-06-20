4e5ee93dee589318e1e5d5178786dc9726423e2f
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-06-20 14:55:51 -0500

    pass an array as props

--- src/App.js
+++ src/App.js
@@ -40,6 +40,34 @@ import "./App.css";
 
 const App = () => {
   return <h1>Hello JSX!</h1>;
+  const List = (props) => {
+    {
+      /* Change code below this line */
+    }
+    return <p>{}</p>;
+    {
+      /* Change code above this line */
+    }
+  };
+
+  class ToDo extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>To Do Lists</h1>
+          <h2>Today</h2>
+          {/* Change code below this line */}
+          <List />
+          <h2>Tomorrow</h2>
+          <List />
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  }
 };
 
 export default App;
